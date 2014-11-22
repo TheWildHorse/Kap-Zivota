902a35d63ef7225f58c4fc1bc86674d38c640820
Igor Rinkovec <shiqqqi@gmail.com>
2014-11-22 12:27:18 +0100

    Added donations seeder.

--- WEB/app/database/seeds/DonationsTableSeeder.php
+++ WEB/app/database/seeds/DonationsTableSeeder.php
@@ -4,15 +4,135 @@ class DonationsTableSeeder extends Seeder {
 
 	public function run()
 	{
-		// Uncomment the below to wipe the table clean before populating
-		 DB::table('donations')->truncate();
+
+		DB::table('donations')->truncate();
 
 		$donations = array(
-        array(''=>)
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+			array(
+				'user_id' => rand(1,7),
+				'institution_id' => rand(1,7),
+				'time' => time(),
+				'measure' => 0.45,
+			),
+
+
 		);
 
-		// Uncomment the below to run the seeder
-		 DB::table('donations')->insert($donations);
+		DB::table('donations')->insert($donations);
 	}
 
 }
