4f075e49ff8700f5a1773ca7df90d6a1a6d0c7c9
tjelcic <tjelcic@foi.hr>
2014-11-22 19:25:09 +0100

    push radi

--- WEB/app/controllers/AdminController.php
+++ WEB/app/controllers/AdminController.php
@@ -15,14 +15,25 @@ class AdminController extends BaseController
     {
 
 
-
-
     }//function index
 
     public function sendPush()
     {
-        $users =  User::where('permission_id','=','0');
-        return View::make('admin.sendpush',compact('users'));
+
+        require_once('PushBots.class.php');
+        $pb = new PushBots();
+        // Application ID
+        $appID = '54705e2f1d0ab18e108b4595';
+        // Application Secret
+        $appSecret = '6f543e1a2b4b3b8d4df958266b006d46';
+        $msg="Ovo će mozda radit";
+        $pb->Alert($msg);
+        $platforms= array(0,1);
+        $pb->Platform($platforms);
+        $pb->App($appID, $appSecret);
+        $pb->Push();
+        /*$users =  User::where('permission_id','=','0');
+        return View::make('admin.sendpush',compact('users'));*/
     }
 
 }
