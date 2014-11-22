102ef3636b2915ca605e25720ba27fb5bc8d37b4
Igor Rinkovec <shiqqqi@gmail.com>
2014-11-22 18:57:36 +0100

    Fixed login.

--- WEB/app/views/users/login.blade.php
+++ WEB/app/views/users/login.blade.php
@@ -1,5 +1,4 @@
-<<<<<<< Updated upstream
-@extends('layouts.scaffold')
+@extends('layouts.scaffoldSuperAdmin')
 
 @section('main')
 
@@ -27,62 +26,24 @@
             <div class="col-lg-10">
               {{ Form::text('email', Input::old('email'), array('class'=>'form-control', 'placeholder'=>'Email')) }}
             </div>
-=======
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <!--<link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css"> -->
-        {{ HTML::style('css/bootstrap.css'); }}
-        <script src="http://code.jquery.com/jquery-latest.min.js"></script>
-        {{ HTML::script('js/bootstrap.js') }}
-        <style>
-            .droplet {
-                background-image: url("{{ asset("img/blood.svg") }}");
-                background-size: 100% 100%;
-                background-repeat: no-repeat;
-                padding: 27% 40% 5% 40%;
-            }
-
-            .btn-login {
-                background-color: #FFF;
-                border: 0px solid;
-
-            }
-        </style>
-    </head>
-
-    <body style="background-image: url('{{ asset("img/light_wool.png") }}');">
-        <div class="droplet">
-            {{ Form::open(array('url' => '/login', 'class' => 'form-horizontal')) }}
-
-                    @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <ul>
-                            {{ implode('', $errors->all('<li class="error">:message</li>')) }}
-                        </ul>
-                    </div>
-                    @endif
-                    <div class="form-group">
-                        <div class="col-lg-12">
-                          {{ Form::text('email', Input::old('email'), array('class'=>'form-control', 'placeholder'=>'Email')) }}
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <div class="col-lg-12">
-                            {{ Form::text('password', Input::old('password'), array('class'=>'form-control', 'placeholder'=>'Password')) }}
-                        </div>
-                    </div>
-                <br/>
-                <div class="form-group">
-                    <div class="col-sm-12" style="text-align: center">
-                      {{ Form::submit('Login', array('class' => 'btn btn-lg btn-login')) }}
-                    </div>
-                </div>
-
-            {{ Form::close() }}
->>>>>>> Stashed changes
         </div>
-    </body>
-</html>
+
+        <div class="form-group">
+            {{ Form::label('password', 'Password:', array('class'=>'col-md-2 control-label')) }}
+            <div class="col-lg-10">
+                {{ Form::text('password', Input::old('password'), array('class'=>'form-control', 'placeholder'=>'Password')) }}
+            </div>
+        </div>
+
+<br/>
+<div class="form-group">
+    <label class="col-sm-5 control-label">&nbsp;</label>
+    <div class="col-sm-7">
+      {{ Form::submit('Login', array('class' => 'btn btn-lg btn-primary')) }}
+    </div>
+</div>
+
+{{ Form::close() }}
+</div>
+@stop
+
